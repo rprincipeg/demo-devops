1680afe41805fed33b01075404e34bd035bfbbde
rprincipeg <renzoprincipeguadiamosprincipe@gmail.com>
2026-07-06 12:00:51 -0500

    Cambio de prueba (falla intencional)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(mensaje="Hola, DevOps!")
+    return jsonify(mensaje="Texto Incorrecto")
 
 @app.route("/salud")
 def salud():
